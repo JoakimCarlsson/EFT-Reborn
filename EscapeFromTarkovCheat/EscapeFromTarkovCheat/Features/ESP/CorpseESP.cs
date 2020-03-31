5488444e2469aa6975fc07fe5549c7e4201512a4
Carlsson <Carlsson@DESKTOP-14GSI35>
2020-03-31 13:29:02 +0100

    Fixed a distance bug

--- EscapeFromTarkovCheat/EscapeFromTarkovCheat/Features/ESP/CorpseESP.cs
+++ EscapeFromTarkovCheat/EscapeFromTarkovCheat/Features/ESP/CorpseESP.cs
@@ -25,7 +25,8 @@ namespace EFT.Reborn
                         if (corpse != null && corpse.gameObject != null && corpse.isActiveAndEnabled)
                         {
                             Vector3 position = corpse.gameObject.transform.position;
-                            float num = GameUtils.InPoint(Main.MainCamera.transform.position, position);
+                            float num = Vector3.Distance(Main.LocalPlayer.Transform.position,
+                                corpse.transform.position);
                             if (num <= Settings.DrawPlayersRange)
                             {
                                 Vector3 screenPosition = GameUtils.WorldPointToScreenPoint(corpse.transform.position);
